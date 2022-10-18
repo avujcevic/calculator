7f1130a5d6ae8682862d6fd32ffa1b914dbc59ee
andrew <a.vujcevic@gmail.com>
2022-10-17 18:59:22 -0600

    Operate addition function with 2 numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
@@ -21,10 +21,27 @@ function operate(func, a, b) {
 const display = document.querySelector("#display")
 const buttons = document.querySelectorAll("button");
 
+let displayValue = 0;
+let storedValue = 0;
+let operator = null;
+
 buttons.forEach((button) => {
     button.addEventListener("click", function(e) {
         if (e.target.className === "number") {
             display.textContent += e.target.textContent;
+            displayValue = display.textContent;
+        } 
+
+        if (e.target.className === "operator") {
+            operator = e.target.id;
+            storedValue = displayValue;
+            display.textContent = "";
+        }
+        
+        if (e.target.id === "btn-equals") {
+            console.log(storedValue);
+            console.log(displayValue);
+            console.log(operate(add, storedValue, displayValue));
         }
     });
 });
